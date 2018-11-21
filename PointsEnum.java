135e4f9612f4f1e278287cab7f82b32f89f2b541
Gillou100 <45231612+Gillou100@users.noreply.github.com>
2018-11-21 13:10:54 +0100

    Update PointsEnum.java

--- PointsEnum.java
+++ PointsEnum.java
@@ -1,65 +1,33 @@
-/* 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet_tennis;
 
-* To change this license header, choose License Headers in Project Properties. 
-
-* To change this template file, choose Tools | Templates 
-
-* and open the template in the editor. 
-
-*/ 
-
-package projet_tennis; 
-
-  
-
-/** 
-
-* 
-
-* @author HUBERT Gilles, TASSART Jean-Florian 
-
-*/ 
-
-public enum PointsEnum 
-
-{ 
-
-    ZERO ("0"), 
-
-    QUINZE ("15"), 
-
-    TRENTE ("30"), 
-
-    QUARANTE ("40"), 
-
-    AVANTAGE ("AV"), 
-
-    GAGNE ("Gagné"); 
-
-     
-
-    private String point; 
-
-     
-
-    PointsEnum(String point) 
-
-    { 
-
-        this.point = point; 
-
-    } 
-
-     
-
-    @Override 
-
-    public final String toString() 
-
-    { 
-
-        return point; 
-
-    } 
-
-} 
+/**
+ *
+ * @author HUBERT Gilles, TASSART Jean-Florian
+ */
+public enum PointsEnum
+{
+    ZERO ("0"),
+    QUINZE ("15"),
+    TRENTE ("30"),
+    QUARANTE ("40"),
+    AVANTAGE ("AV"),
+    GAGNE ("Gagné");
+    
+    private String point;
+    
+    PointsEnum(String point)
+    {
+        this.point = point;
+    }
+    
+    @Override
+    public final String toString()
+    {
+        return point;
+    }
+}
