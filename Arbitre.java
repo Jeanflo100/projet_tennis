f93c3e0483f1c78dc6b86ccf0494ea2bbf6b6833
Gillou100 <45231612+Gillou100@users.noreply.github.com>
2018-11-21 13:20:55 +0100

    Update Arbitre.java

--- Arbitre.java
+++ Arbitre.java
@@ -1,18 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package projet_tennis;
 
-
-public class Arbitre {
+/**
+ *
+ * @author HUBERT Gilles, TASSART Jean-Florian
+ */
+public final class Arbitre extends Personne
+{
+    public Arbitre()
+    {
+        super();
+    }
     
-    public void annoncerScore() {
+    public final void ennoncerServeur(Joueur joueur)
+    {
+        parler("Service : " + joueur.getNom());
+    }
+    public final void ennoncerScore(Score score)
+    {
+        parler(score);
+    }
+    public final void signalerFaute()
+    {
+        
+    }
+    public final void repondAuJoueur()
+    {
         
     }
     
-    public void signalerFaute(){
-        
+    @Override
+    public final void parler(Object texte)
+    {
+        parler("Arbitre " + getNom().toUpperCase(), texte);
     }
-    
-    public void repondAuJoueur(){
-        
-    }
-    
 }
