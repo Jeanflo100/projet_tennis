389a9646a964decf69f178b1d9187f1c50c15f35
Gillou100 <45231612+Gillou100@users.noreply.github.com>
2018-11-21 13:07:57 +0100

    Update Jeu.java

--- Jeu.java
+++ Jeu.java
@@ -7,8 +7,30 @@ package projet_tennis;
 
 /**
  *
- * @author Jeanflo
+ * @author HUBERT Gilles, TASSART Jean-Florian
  */
-public class Jeu {
+public final class Jeu
+{
+    Score score = new Score<>(new Points(), new Points());
+    private final Joueur serveur;
+    private final Joueur receveur;
+    private final Arbitre arbitre;
     
+    public Jeu(Joueur joueur1, Joueur joueur2, Arbitre arbitre)
+    {
+        serveur = joueur1;
+        receveur = joueur2;
+        this.arbitre = arbitre;
+    }
+    
+    public final void jouer()
+    {
+        arbitre.ennoncerServeur(serveur);
+        echange();
+    }
+    
+    public final void echange()
+    {
+        arbitre.ennoncerScore(score);
+    }
 }
