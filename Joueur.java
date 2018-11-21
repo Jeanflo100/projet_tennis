1e602c22f86843aafee33907fbafb4afaf6112f4
Gillou100 <45231612+Gillou100@users.noreply.github.com>
2018-11-21 13:19:05 +0100

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -1,7 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package projet_tennis;
 
-
-
+/**
+ *
+ * @author HUBERT Gilles, TASSART Jean-Florian
+ */
 public class Joueur extends Personne {
     
     protected Main main;
@@ -9,46 +16,61 @@ public class Joueur extends Personne {
     protected Integer classement;
     protected String entraineur;
     
-    public Joueur(){
-        
+    public Joueur()
+    {
+        super();
     }
     
-    public void setMain(Main main){
+    public void setMain(Main main)
+    {
         this.main = main;
     }
     
-    public void setSponsor(Sponsor[] sponsor){
+    public void setSponsor(Sponsor[] sponsor)
+    {
         int nb_sponsor = sponsor.length;
         this.sponsor = new Sponsor[nb_sponsor];
         System.arraycopy(sponsor, 0, this.sponsor, 0, nb_sponsor);
     }
     
-    public void setClassement(Integer classement){
+    public void setClassement(Integer classement)
+    {
         this.classement = classement;
     }
     
-    public void setEntraineur(String entraineur){
+    public void setEntraineur(String entraineur)
+    {
         this.entraineur = entraineur;
     }
     
-    public Main getMain(){
+    public Main getMain()
+    {
         return(this.main);
     }
     
-    public Sponsor[] getSponsor(){
+    public Sponsor[] getSponsor()
+    {
         return(this.sponsor);
     }
     
-    public Integer getClassement(){
+    public Integer getClassement()
+    {
         return(this.classement);
     }
     
-    public String getEntraineur(){
+    public String getEntraineur()
+    {
         return(this.entraineur);
     }
     
-    public void changementTenue(){
-        System.out.println("Je change de tenue !");
+    public void changementTenue()
+    {
+        parler("Je change de tenue !");
     }
     
+    @Override
+    public final void parler(Object texte)
+    {
+        parler("Joueur " + getNom().toUpperCase(), texte);
+    }
 }
